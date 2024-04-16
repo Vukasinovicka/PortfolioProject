c538c95e217d370a269dcd42a7d934559e99ea79
Vukasinovicka <144479169+Vukasinovicka@users.noreply.github.com>
2024-04-16 02:30:36 +0200

    Delete src/test/java/utlilities directory

--- src/test/java/utlilities/ConstantValue.java
+++
@@ -1,9 +0,0 @@
-package utlilities;
-
-public class ConstantValue {
-
-    public static final String userName = "v.ana.95@hotmail.com";
-    public static final String password = "Vaki@1504";
-
-
-}
